464c002d1885b1f8a5ad867503f722af363abfa3
Noa Figenblat <noa.figenblat@rain.us>
2023-10-05 23:30:56 -0500

    finished all steps - still debugging the search process and the keys for the interests

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,27 +1,241 @@
-import { Button, Container, Form, Row } from "react-bootstrap";
+import { Button, Container, Form, Row, Col} from "react-bootstrap";
+import { useState, useEffect } from 'react';
+import Student from "./Student";
+import Pagination from 'react-bootstrap/Pagination';
 
-const Classroom = () => {
+
+  const Classroom = () => {
+   
+    
+    let numStudents = 0;
+    const[students, setStudents] = useState([]);
+    const[inputName, setInputName] = useState("");
+    const[inputMajor, setInputMajor] = useState("");
+    const[inputInterest, setInputInterest] = useState("");
+    const[shownStudents, setShownStudents] = useState([]);
+    const [activePage, setActivePage] = useState(1);
+
+
+    useEffect(()=>{
+        fetch("https://cs571.org/api/f23/hw4/students", {
+        method: "GET",
+        headers: {
+            "X-CS571-ID": CS571.getBadgerId()
+        }
+     })
+        .then(response => response.json())
+        .then(data => {
+            setStudents(data)
+            setShownStudents(data)
+        })
+    },[])
+        console.log(students);
+        numStudents = students.length;
+
+    
+    const buildPaginator =() => {
+        let pages =[]
+
+        pages.push(
+            <Pagination.First
+            active = {activePage === 1}
+            onClick={()=>setActivePage(1)}
+            />
+        )
+        pages.push(
+            <Pagination.Prev
+            active={activePage === activePage - 1}
+            onClick={()=> setActivePage(activePage-1)}
+            />
+        )
+
+
+        const num_pages = Math.ceil(shownStudents.length/24)
+        for(let i = 1; i <= num_pages; i++){
+            pages.push(
+                <Pagination.Item
+                key={i}
+                active= {activePage === i}
+                onClick={()=>setActivePage(i)}
+                >
+                    {i}
+                </Pagination.Item>
+            )
+        }
+
+        pages.push(
+            <Pagination.Next
+            active={activePage === activePage + 1}
+            onClick={()=> setActivePage(activePage+1)}
+            />
+        )
+        pages.push(
+            <Pagination.Last
+            active={activePage === num_pages}
+            onClick={()=> setActivePage(num_pages)}
+            />
+        )
+        return pages;
+
+    }
+
+
+
+    useEffect(() => {
+        console.log("inputName, inputMajor, or inputInterest changed!");
+        filterStudents();
+        }, [inputName, inputMajor, inputInterest]);
+
+
+    useEffect(() => {
+        console.log("shownStudents changed!");
+        console.log(shownStudents);
+        }, [shownStudents]);
+
+
+    function filterStudents(){
+        let filteredStudents =[];
+
+
+        for(let i = 0; i < students.length; i++){
+
+            // just searching by name 
+            if(inputName != "" && inputMajor === "" && inputInterest ===""){
+                if(students[i].name.first.toLowerCase().includes(inputName) || students[i].name.last.toLowerCase().includes(inputName)){
+                    filteredStudents.push(students[i])
+                   }
+            }
+
+            // just searching by major 
+            else if(inputName === "" && inputMajor != "" && inputInterest ===""){
+                if(students[i].major.toLowerCase().includes(inputMajor)){
+                    filteredStudents.push(students[i])
+                }
+
+            }
+
+            // just searching by interest 
+            else if(inputName === "" && inputMajor === "" && inputInterest !=""){
+                for(let j = 0; j<students[i].interests.length; j++){
+                    if(students[i].interests[j].toLowerCase().includes(inputInterest)){
+                        filteredStudents.push(students[i])
+                    }
+
+                }
+
+            }
+
+            //  searching by name and major 
+            else if(inputName != "" && inputMajor != "" && inputInterest === ""){
+                if(students[i].major.toLowerCase().includes(inputMajor) && (students[i].name.first.toLowerCase().includes(inputName) || students[i].name.last.toLowerCase().includes(inputName))){
+                    filteredStudents.push(students[i])
+                }
+            }
+
+            // searching by name and interest 
+            else if(inputName != "" && inputMajor === "" && inputInterest != ""){
+                if(students[i].name.first.toLowerCase().includes(inputName) || students[i].name.last.toLowerCase().includes(inputName)){
+                    for(let j = 0; j < students[i].interests.length; j++){
+                        if(students[i].interests[j].toLowerCase().includes(inputInterest)){
+                            filteredStudents.push(students[i]);
+                        }
+                    }
+                }
+            }
+
+            // searching by major and interest 
+            else if(inputName === "" && inputMajor != "" && inputInterest != "" ){
+                if(students[i].major.toLowerCase().includes(inputMajor)){
+                    for(let j = 0; j < students[i].interests.length; j++){
+                        if(students[i].interests[j].toLowerCase().includes(inputInterest)){
+                            filteredStudents.push(students[i])
+                        }
+                    }
+                }
+            }
+
+            // searching by name, major, and interest 
+            else if(inputName != "" && inputMajor != "" && inputInterest != "" ){
+                if(students[i].major.toLowerCase().includes(inputMajor) && (students[i].name.first.toLowerCase().includes(inputName) || students[i].name.last.toLowerCase().includes(inputName))){
+                    for(let j = 0; j < students[i].interests.length; j++){
+                        if(students[i].interests[j].toLowerCase().includes(inputInterest)){
+                            filteredStudents.push(students[i])
+                        }
+                    }
+                }
+            }
+            else if(inputName ==="" && inputMajor === "" && inputInterest ===""){
+                filteredStudents.push(students[i])
+            }
+         }
+
+        
+        setShownStudents(filteredStudents);
+        
+        
+    }
+
+    numStudents = shownStudents.length;
+
+
+    function resets(){
+        setShownStudents(students)
+        numStudents = students.length;
+    }
+    
+    
+   
     return <div>
         <h1>Badger Book - Fall 2023</h1>
         <p>Search for students below!</p>
         <hr />
         <Form>
             <Form.Label htmlFor="searchName">Name</Form.Label>
-            <Form.Control id="searchName"/>
+            <Form.Control 
+            id="searchName"
+            value ={inputName}
+            onChange={(e) => setInputName(e.target.value.toLowerCase())}
+            />
+
             <Form.Label htmlFor="searchMajor">Major</Form.Label>
-            <Form.Control id="searchMajor"/>
+            <Form.Control 
+            id="searchMajor"
+            value ={inputMajor}
+            onChange={(e) => setInputMajor(e.target.value.toLowerCase())}  
+            />
+
             <Form.Label htmlFor="searchInterest">Interest</Form.Label>
-            <Form.Control id="searchInterest"/>
+            <Form.Control 
+            id="searchInterest"
+            value ={inputInterest}
+            onChange={(e) => setInputInterest(e.target.value.toLowerCase())}
+            />
+
             <br />
-            <Button variant="neutral">Reset Search</Button>
+            <Button variant="neutral" onClick={resets}>Reset Search</Button>
         </Form>
+
+        <p> There are {numStudents} student(s) matching your search.</p>
+        
         <Container fluid>
-            <Row>
+            <Row> 
                 { /* TODO Students go here! */ }
+                {
+                   shownStudents.slice((activePage - 1) * 24, activePage*24).map(studs => <Col key={studs.id} xs={12} sm={6} md={4} lg={3} xl={2}><Student {...studs}/></Col>)
+                }
             </Row>
         </Container>
+        <br/>
+        <Pagination>
+            {buildPaginator()}
+        </Pagination>
+            
     </div>
 
+    
+
 }
+
+
 
 export default Classroom;
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,8 +1,24 @@
+
 const Student = (props) => {
+
+    
+    let listArr = []
+    for(let i = 0; i < props.interests.length; i++){
+        listArr.push(<li key = {props.interests[i]}>{props.interests[i]}</li>)
+    }
+
+
     return <div>
         <h2>{props.name.first} {props.name.last}</h2>
         {/* TODO Student data goes here! */}
+        <h6>Majoring in: {props.major}</h6>
+        <h6>Enrolled in {props.numCredits} credits</h6>
+        <h6>From Wisconsin: {props.fromWisconsin.toString()}</h6>
+        <h6>{props.interests.length} interests:</h6>
+        <ul> {listArr} </ul>
     </div>
+
+    
 }
 
 export default Student;
